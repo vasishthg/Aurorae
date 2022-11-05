c86be559dde23c0af6fc059536aa4f9dd7428c97
Vasishth Gupta <76578694+vasishthg@users.noreply.github.com>
2022-11-05 20:34:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from urllib import parse
-TOKEN = "MTAxMTYyODI1NDY3MjcyODA3NQ.GeqL2V.Jx0Dq_d8xVUKg-PPc6NejAm7hHNIJ9JangfEP4"
-CLIENT_SECRET = "IrKPmkmIXFW11o31f4KcgGad9Aqh6ly-"
+TOKEN = "DISCORD_TOKEN"
+CLIENT_SECRET = "CLIENT_SECRET"
 REDIRECT_URI = "http://127.0.0.1:5000/oauth/callback"
-OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id=1011628254672728075&redirect_uri={parse.quote(REDIRECT_URI)}&response_type=code&scope=identify"
+OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id=CLIENT_ID&redirect_uri={parse.quote(REDIRECT_URI)}&response_type=code&scope=identify"
